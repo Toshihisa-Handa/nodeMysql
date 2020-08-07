30e50e436bc9d278134afbbb724fd18a1eae6c72
Toshi <waykiyoharu@gmail.com>
2020-08-07 14:57:27 +0900

    テーブルの作成

--- app.js
+++ app.js
@@ -12,17 +12,25 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: ''
+  password: '',
   database:'express_db'
 });
 
+//データベース接続
 con.connect(function(err) {
   if (err) throw err;
   console.log('Connected');
   //データベースの作成と接続(この記述でデータベースが作られる。記述後ターミナルでサーバーが更新されるとデータベース不要になるので消す)
-  con.query('CREATE DATABASE express_db', function(err, result){
+//   con.query('CREATE DATABASE express_db', function(err, result){
+//       if(err)throw err;
+//       console.log('database createde')
+//   });
+
+//テーブル作成の記述（データベース同様読み込まれると不要になる）
+  const sql ='CREATE TABLE users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL)';
+  con.query(sql, function(err, result){
       if(err)throw err;
-      console.log('database createde')
+      console.log('table created');
   });
 });
 
