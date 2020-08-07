2d5064a503b0d863c0bc2283d260726d3fd2f9ec
Toshi <waykiyoharu@gmail.com>
2020-08-07 14:55:45 +0900

    express接続

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+//expressからmysqlの接続
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: ''
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected');
+});
+
 app.get('/', (req, res) => res.send('Hello Worldaa!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
