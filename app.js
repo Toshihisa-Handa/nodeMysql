b6097c6c7f75c21451da5e59329a23578db37f4b
Toshi <waykiyoharu@gmail.com>
2020-08-07 17:27:37 +0900

    入力フォームから登録処理を実装

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ const con = mysql.createConnection({
   database:'express_db'
 });
 
-//データベース接続
-con.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected');
+//読み込んだbodyParserをミドルウェアとして設定
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+//データベース接続(最初の環境設定（DBやtableに使用した後は不要)
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log('Connected');
 
 //データベースの作成と接続(この記述でデータベースが作られる。記述後ターミナルでサーバーが更新されるとデータベース不要になるので消す)
 //   con.query('CREATE DATABASE express_db', function(err, result){
@@ -46,6 +50,21 @@ con.connect(function(err) {
 
   //insert文にて登録の実行
   //エディタが更新されるたびに実行されるので、１回実行したらコメントアウト or 削除する
+   //パターン①
+//    const sql = "INSERT INTO users(name,email) VALUES('kevin','kevin@test.com')"
+//    con.query(sql,function(err, result, fields){
+// 	if (err) throw err;
+// 	console.log(result)
+// })
+
+    //パターン②
+// const sql = "INSERT INTO users(name,email) VALUES(?,?)"
+// con.query(sql,['Jack','jack@exsample.co.jp'],function(err, result, fields){
+//     if (err) throw err;
+//     console.log(result)
+// })
+
+    //パターン③
 // const sql = "INSERT INTO users SET ?"
 // con.query(sql,{name:'Tommy', email:'Tommy@test.com'},function(err, result, fields){
 // 	if (err) throw err;
@@ -53,7 +72,7 @@ con.connect(function(err) {
 // })
 
 
-});
+// });
 
 
 //select文で取得したデータをブラウザで表示
@@ -67,8 +86,7 @@ con.connect(function(err) {
 
 // })
 
-//読み込んだbodyParserをミドルウェアとして設定
-app.use(bodyParser.urlencoded({ extended: true}));
+
 
 
 //localhost:3000で呼び出されるルートの定義
@@ -78,7 +96,17 @@ app.get('/', (req, res) =>
     // res.sendFile(path.join(`${__dirname}/html/form.html`)))↑と同じ意味
     
     //req, resにインプットタグから送信されたデータが入っていて、送信された内容がreq.bodyで表示される
-app.post('', (req, res) => res.send(req.body))
+app.post('/', (req, res) => {
+	const sql = "INSERT INTO users SET ?"
+
+	con.query(sql,req.body,function(err, result, fields){
+		if (err) throw err;
+		console.log(result);
+		res.send('登録が完了しました');
+
+	});
+});
+
 
 //サーバーの接続状態の確認用コンソール
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
