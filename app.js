969744fcc36a23f2c3ad20375f7937ddff243072
Toshi <waykiyoharu@gmail.com>
2020-08-07 14:56:43 +0900

    データベースの作成

--- app.js
+++ app.js
@@ -13,11 +13,17 @@ const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: ''
+  database:'express_db'
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log('Connected');
+  //データベースの作成と接続(この記述でデータベースが作られる。記述後ターミナルでサーバーが更新されるとデータベース不要になるので消す)
+  con.query('CREATE DATABASE express_db', function(err, result){
+      if(err)throw err;
+      console.log('database createde')
+  });
 });
 
 app.get('/', (req, res) => res.send('Hello Worldaa!'))
